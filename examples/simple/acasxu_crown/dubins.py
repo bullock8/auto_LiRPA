b0a983b80cbf8e49068569aa9f308a79ace30b3d
Alex Yuan <alexyuan0.ay@gmail.com>
2025-02-20 18:19:17 -0600

    basic simulation loop done

--- examples/simple/acasxu_crown/dubins.py
+++ examples/simple/acasxu_crown/dubins.py
@@ -12,6 +12,8 @@ from auto_LiRPA import BoundedTensor
 from verse.utils.utils import wrap_to_pi
 import numpy as np 
 import torch
+from collections import deque
+
 
 class AgentMode(Enum):
     COC = auto()
@@ -35,8 +37,7 @@ def get_acas_state(own_state: np.ndarray, int_state: np.ndarray) -> torch.Tensor
     psi = wrap_to_pi(int_state[2]-own_state[2])
     return torch.tensor([dist, theta, psi, own_state[3], int_state[3]])
 
-def get_final_states_sim(trace) -> Tuple[List]: 
-    n = trace.nodes[0]
+def get_final_states_sim(n) -> Tuple[List]: 
     own_state = n.trace['car1'][-1]
     int_state = n.trace['car2'][-1]
     return own_state, int_state
@@ -58,17 +59,48 @@ if __name__ == "__main__":
         # initial_state=[[15, -0.3, 0, 0.5], [15, 0.3, 0, 0.5]],
         initial_mode=(AgentMode.COC, TrackMode.T1)
     )
+    T = 5
+    Tv = 1
+    ts = 0.1
+
     scenario.add_agent(car)
     scenario.add_agent(car2)
-    trace = scenario.simulate(0.2, 0.1)
-    own_state, int_state = get_final_states_sim(trace)
-    print(own_state, int_state)
-    acas_state = get_acas_state(own_state[1:], int_state[1:])
+    trace = scenario.simulate(Tv, ts) # this is the root
+    id = 1+trace.root.id
+    net = 0 # eventually this could be modified in the loop by some cmd_list var
+    model = torch.load(f"./examples/simple/acasxu_crown/ACASXU_run2a_{net + 1}_1_batch_2000.pth")
+    queue = deque()
+    queue.append(trace.root) # queue should only contain ATNs  
+    ### begin looping
+    while len(queue):
+        cur_node = queue.popleft() # equivalent to trace.nodes[0] in this case
+        own_state, int_state = get_final_states_sim(cur_node)
+        # print(own_state, int_state)
+        acas_state = get_acas_state(own_state[1:], int_state[1:]).float()
+        ads = model(acas_state.view(1,5)).detach().numpy()
+        new_mode = np.argmax(ads[0]) # will eventually be a list
+        # print(ads, new_mode, ads[0][new_mode])
 
-    net = 0
-    model = torch.load(f"../ACASXU_run2a_{net + 1}_1_batch_2000.pth")
-    print(model(acas_state))
-    exit()
+        # this will eventually be a loop
+        scenario.set_init(
+            [[own_state[1:], own_state[1:]], [int_state[1:], int_state[1:]]], # this should eventually be a range 
+            [(AgentMode(new_mode), TrackMode.T1),(AgentMode.COC, TrackMode.T1)]
+        )
+        id += 1
+        new_trace = scenario.simulate(Tv, ts)
+        temp_root = new_trace.root
+        new_node = cur_node.new_child(temp_root.init, temp_root.mode, temp_root.trace, cur_node.start_time + Tv, id)
+        cur_node.child.append(new_node)
+        if new_node.start_time + Tv>=T: # if the time of the current simulation + start_time is at or above total time, don't add
+            continue
+        # print(new_node.start_time + Tv)
+        queue.append(new_node)
+
+    trace.nodes = trace._get_all_nodes(trace.root)
+    print(len(trace.nodes))
+    fig = go.Figure()
+    fig = simulation_tree(trace, None, fig, 1, 2, [1, 2], "fill", "trace")
+    fig.show()
     # trace = scenario.verify(0.2,0.1) # increasing ts to 0.1 to increase learning speed, do the same for dryvr2
     # fig = reachtube_tree(trace) 
     # fig.show() 
